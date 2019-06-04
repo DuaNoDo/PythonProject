df5443ebf01f86fa6a661a4f15fe6dcc6609cf2e
WON <teadone@naver.com>
2019-06-04 15:23:03 +0900

    병합

--- flask11_5.py
+++ flask11_5.py
@@ -1,63 +1,137 @@
-import json
 import sqlite3
 
-from flask import Flask
-from flask import request
+from flask import Flask, render_template, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-app.debug=True
+app.debug = True
 
-def get_db_con() ->sqlite3.connect:
-    return sqlite3.connect("db.sqlite")
+
+def get_db_con() -> sqlite3.connect:
+    return sqlite3.connect("movie.db")
+
 
 @app.route("/")
-
-def hello():
+def index():
     with get_db_con() as con:
-        cur= con.cursor()
+        cur = con.cursor()
 
-        q="select * from hanbit_books"
-        result=cur.execute(q)
+        mov_info_all = "select * from Mov_info"
 
-    result_json=jsonize(result)
-    return result_json
+        mov_info_all = cur.execute(mov_info_all)
+        mov_info_all = list(mov_info_all.fetchall())
 
-@app.route("/books/by/author")
+        action = "select * from mov_info where mov_info like '%액션%' order by mov_date desc"
 
-def get_books_by_author():
-    name=request.args.get("name")
+        mov_info_action = cur.execute(action.format('action'))
+        mov_info_action = list(mov_info_action.fetchall())
+
+        romance = "select * from mov_info where mov_info like '%로맨스%' order by mov_date desc"
+
+        mov_info_romance = cur.execute(romance.format('romance'))
+        mov_info_romance = list(mov_info_romance.fetchall())
+
+        horror = "select * from mov_info where mov_info like '%공포%' order by mov_date desc"
+
+        mov_info_horror = cur.execute(horror.format('horror'))
+        mov_info_horror = list(mov_info_horror.fetchall())
+
+        ani = "select * from mov_info where mov_info like '%애니메이션%' order by mov_date desc"
+
+        mov_info_ani = cur.execute(ani.format('ani'))
+        mov_info_ani = list(mov_info_ani.fetchall())
+
+        url = 'http://www.kobis.or.kr/'
+
+        return render_template('index.html', mov_info_all=mov_info_all, url=url, mov_info_action=mov_info_action,
+                               mov_info_romance=mov_info_romance, mov_info_horror=mov_info_horror,
+                               mov_info_ani=mov_info_ani)
+
+
+@app.route('/catalog/')
+@app.route('/catalog', methods=['POST', 'GET'])
+@app.route('/catalog/', methods=['POST', 'GET'])
+def catalog():
     with get_db_con() as con:
-        cur=con.cursor()
-        q="select * from hanbit_books where author like :name order by title"
-        param={
-            "name":"%"+name+"%"
-        }
-        result=cur.execute(q,param)
-    result_json=jsonize(result)
-    return result_json
+        cur = con.cursor()
 
-@app.route("/books/by/month")
+        mov_info_all = "select * from Mov_info"
 
-def get_books_by_month():
-    month=request.args.get("month")
-    if int(month)<10:
-        month="0"+month
+        mov_info_all = cur.execute(mov_info_all)
+        mov_info_all = list(mov_info_all.fetchall())
 
+        action = "select * from mov_info where mov_info like '%액션%' order by mov_date desc"
+
+        mov_info_action = cur.execute(action.format('action'))
+        mov_info_action = list(mov_info_action.fetchall())
+
+        romance = "select * from mov_info where mov_info like '%로맨스%' order by mov_date desc"
+
+        mov_info_romance = cur.execute(romance.format('romance'))
+        mov_info_romance = list(mov_info_romance.fetchall())
+
+        horror = "select * from mov_info where mov_info like '%공포%' order by mov_date desc"
+
+        mov_info_horror = cur.execute(horror.format('horror'))
+        mov_info_horror = list(mov_info_horror.fetchall())
+
+        ani = "select * from mov_info where mov_info like '%애니메이션%' order by mov_date desc"
+
+        mov_info_ani = cur.execute(ani.format('ani'))
+        mov_info_ani = list(mov_info_ani.fetchall())
+
+        url = 'http://www.kobis.or.kr/'
+
+        total_all = 'select count(*) from mov_info'
+        total_all = cur.execute(total_all)
+        total_all = int(total_all.fetchone()[0])
+
+        return render_template('catalog.html', mov_info_all=mov_info_all, url=url, mov_info_action=mov_info_action,
+                               mov_info_romance=mov_info_romance, mov_info_horror=mov_info_horror,
+                               mov_info_ani=mov_info_ani, total_all=total_all, )
+
+
+@app.route('/details')
+@app.route('/details/<int:mov_code>/')
+@app.route('/details/<int:mov_code>')
+def details(mov_code):
     with get_db_con() as con:
-        cur=con.cursor()
-        q="select * from hanbit_books where strftime('%m',pub_date) = :month order by pub_date desc"
-        param={
-            "month":month
-        }
-        result=cur.execute(q,param)
-    result_json=jsonize(result)
-    return result_json
+        cur = con.cursor()
 
+        mov_info_details = "select * from Mov_info where mov_code = {}"
+
+        mov_info_details = cur.execute(mov_info_details.format(mov_code))
+        mov_info_details = list(mov_info_details.fetchall())
+
+        # CINEMA
+        mov_score_cinema = "select * from mov_score where mov_code = {} and rep_site='cinema'"
+
+        mov_score_cinema = cur.execute(mov_score_cinema.format(mov_code))
+        mov_score_cinema = list(mov_score_cinema.fetchall())
+
+        # NAVER
+        mov_score_naver = "select * from mov_score where mov_code = {} and rep_site='naver'"
+
+        mov_score_naver = cur.execute(mov_score_naver.format(mov_code))
+        mov_score_naver = list(mov_score_naver.fetchall())
+
+        # MEGABOX
+        mov_score_mega = "select * from mov_score where mov_code = {} and rep_site='megabox'"
+
+        mov_score_mega = cur.execute(mov_score_mega.format(mov_code))
+        mov_score_mega = list(mov_score_mega.fetchall())
+
+    url = 'http://www.kobis.or.kr/'
+
+    return render_template('details.html', mov_info_details=mov_info_details, mov_score_cinema=mov_score_cinema,
+                           mov_score_naver=mov_score_naver, mov_score_mega=mov_score_mega, url=url)
+
+
+'''
 def jsonize(result):
     result_json=json.dumps(list(result.fetchall()),ensure_ascii=False).encode("utf-8")
     return result_json
+'''
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
-
