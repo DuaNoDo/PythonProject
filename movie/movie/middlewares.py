80c99be63df85e6f1e948289b1d147e9a4d63a63
WON <teadone@naver.com>
2019-06-14 15:53:29 +0900

    리드미 수정3

--- movie/movie/middlewares.py
+++ movie/movie/middlewares.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import sqlite3
 
-path = '/Desktop/PythonProject/chromedriver.exe'
+path = '/chromedriver.exe'
 options = webdriver.ChromeOptions()
 #options.add_argument('headless')
 options.add_argument('window-size=1200x600')
